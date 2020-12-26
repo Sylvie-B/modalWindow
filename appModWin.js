86b025515bc632160da2cde9820df40795345ac9
Sylvie-B <unsolo@hotmail.fr>
2020-12-26 21:36:30 +0100

    close window

--- appModWin.js
+++ appModWin.js
@@ -8,6 +8,7 @@ let ModWin = function (bgWinWidth, bgWinHeight, bgWinCol, modWinWidth, modWinHei
 
     this.open = function () {
         let newBgWin = document.createElement('div');
+        newBgWin.id = 'modalWin';
         newBgWin.style.width = this.bgW +  "px";
         newBgWin.style.height = this.bgH + "px";
         newBgWin.style.paddingTop = (this.bgH-this.modH)/2 + 'px';
@@ -20,11 +21,13 @@ let ModWin = function (bgWinWidth, bgWinHeight, bgWinCol, modWinWidth, modWinHei
         newModWin.style.height = this.modH + "px";
         newModWin.style.margin = 'auto';
         newModWin.style.backgroundColor = this.modC;
+        newModWin.style.border = '3px black double';
         newBgWin.appendChild(newModWin);
+        newModWin.addEventListener('click', () => newBgWin.remove());
     }
-
 }
 
-let modBg1 = new ModWin('1200', '800', 'rgb(255 177 0 / 50%)', '500', '150', 'white');
+let modBg1 = new ModWin('1200', '800', 'rgb(255 177 0 / 25%)', '500', '300', 'white');
 
 modBg1.open();
+
